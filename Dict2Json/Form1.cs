4b5cda087a248723cfb413d1c20cd15c22a94a5a
aucan <aucan@AUCAN>
2018-04-02 16:45:58 +0300

    ok

--- Dict2Json/Form1.cs
+++ Dict2Json/Form1.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic; 
 using System.Windows.Forms;
 
 namespace Dict2Json
@@ -15,6 +10,39 @@ namespace Dict2Json
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public class myclass
+        {
+            public string item1;
+            public int item2;
+            public DateTime item3;
+
+            public myclass(string _item1, int _item2, DateTime _item3)
+            {
+                item1 = _item1;
+                item2 = _item2;
+                item3 = _item3;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dictionary<UInt32, myclass> dict = new Dictionary<UInt32, myclass>();
+            for (int i = 0; i < 2; i++)
+            {
+                dict.Add((UInt32)i, new myclass(i.ToString(),i,DateTime.Now));
+            }
+
+            string json = JsonConvert.SerializeObject(dict, Formatting.Indented);
+            MessageBox.Show(json);
+
+            Dictionary<string, myclass> dict2 = JsonConvert.DeserializeObject < Dictionary <string, myclass>>(json);
+           
+            foreach (var item in dict2)
+            {
+                MessageBox.Show(item.Value.item1, item.Key);
+            }
         }
     }
 }
